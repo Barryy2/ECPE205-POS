fe747d66160b3913b4904b7b8d286fc2384e1d98
barry2 <barrylopez122@gmail.com>
2026-02-17 16:57:26 +0800

    Start order screeb

--- src/CodeScreen.java
+++ src/CodeScreen.java
@@ -1,16 +1,62 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class CodeScreen extends JFrame {
 
     CodeScreen(){
 
+    JTextField Inputsku = new JTextField(20);
+    JButton Enterbutton = new JButton("Enter");
+    JLabel skuinput = new JLabel("Input Product: ");
 
 
-        setTitle("Code Screen");
+        setLayout(new GridBagLayout());
+        //Top bar level
+     addComponent(1,0,Inputsku);
+     addComponent(0,0,skuinput);
+     addComponent(3,0,Enterbutton);
+
+
+
+        setTitle("Order Screen");
         setSize(300,400);
         JFrame jFrame = new JFrame();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        pack();
+    }
+
+
+    private  void addComponent (int x , int y , int width , Component c   ){
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = x;
+        constraints.gridy = y;
+        constraints.gridwidth = width;
+        add(c ,constraints);
+        constraints.anchor = GridBagConstraints.CENTER;
+
+    }
+
+    private void addComponent (int x , int y , Component c   ){
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = x;
+        constraints.gridy = y;
+//        constraints.gridwidth = width;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(c ,constraints);
+
+    }
+
+    private void addComponent (int x , int y , int width , int fill , Component c   ){
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = x;
+        constraints.gridy = y;
+        constraints.gridwidth = width;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(c ,constraints);
+
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import javax.swing.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main{
-    static void main() {
+
+    public static void main(String[] args) {
 
             ProductScreen productScreen = new ProductScreen();
             CodeScreen CodeScreen = new CodeScreen();
